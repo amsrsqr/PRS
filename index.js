ae14bc4808b990b062e05d9ddc5ae87bd427e5d3
Amol Rashinkar <74755561+amsrsqr@users.noreply.github.com>
2022-11-14 17:28:35 +0530

    src

---
+++ index.js
@@ -0,0 +1,50 @@
+// ** React Imports
+import { Suspense, lazy } from 'react'
+import ReactDOM from 'react-dom'
+
+// ** Toast & ThemeColors Context
+// import { ToastContainer } from 'react-toastify'
+import { ThemeContext } from './utility/context/ThemeColors'
+
+// ** Spinner (Splash Screen)
+import Spinner from './@core/components/spinner/Fallback-spinner'
+
+// ** Ripple Button
+import './@core/components/ripple-button'
+
+// ** PrismJS
+import 'prismjs'
+import 'prismjs/themes/prism-tomorrow.css'
+import 'prismjs/components/prism-jsx.min'
+
+// ** React Perfect Scrollbar
+import 'react-perfect-scrollbar/dist/css/styles.css'
+
+// ** React Toastify
+// import '@styles/react/libs/toastify/toastify.scss'
+
+// ** Core styles
+import './@core/assets/fonts/feather/iconfont.css'
+import './@core/scss/core.scss'
+import './assets/scss/style.scss'
+// ** Service Worker
+import * as serviceWorker from './serviceWorker'
+import { Toaster } from 'react-hot-toast'
+
+// ** Lazy load app
+const LazyApp = lazy(() => import('./App'))
+ReactDOM.render(
+    
+    <Suspense fallback={<Spinner />}>
+      <ThemeContext>
+        <LazyApp />
+          <Toaster />
+      </ThemeContext>
+    </Suspense>,
+  document.getElementById('root')
+)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
